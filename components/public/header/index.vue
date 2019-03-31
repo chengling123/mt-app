15d0cd6fc762cb251a629a98c2dc1a9bc530816b
chenglingling <1192374211@qq.com>
2019-03-31 11:07:59 +0800

    first

---
+++ components/public/header/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-row class="m-header" :gutter="15">
+    <el-col :span="6">
+      <geo/>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content bg-purple"></div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content bg-purple"></div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content bg-purple"></div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import Geo from "./geo.vue"
+export default {
+  data() {
+    return {};
+  },
+
+  components: {
+      Geo
+  },
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+};
+</script>
+<style lang='scss' scoped>
+</style>
